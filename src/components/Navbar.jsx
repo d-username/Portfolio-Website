a752c979662f1fa199c9a75aa98bb4e98d2bccc1
David <czdavid93@gmail.com>
2022-08-24 16:00:47 +0200

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,7 +47,7 @@ export function Navbar() {
             </a>
           </li>
         </ul>
-        <button>
+        <button id="button-resume">
           <NavLink to='/resume'>Resume</NavLink>
         </button>
       </nav>
