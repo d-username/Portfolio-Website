29dc19d955b92b4c362d1dadfbae8442946511e2
David <czdavid93@gmail.com>
2022-08-27 13:45:01 +0200

    adding google map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,17 @@
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 import './map.css';
 
 export function Map() {
-  return(
-    <div id='map'>
-      hello map
-    </div>
-  )
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  });
+
+  if (!isLoaded) return <div>Loading...</div>
+
+  return <TheMap />;
+}
+
+
+function TheMap() {
+  return <GoogleMap zoom={10} center={{lat:44, lng: -80}} mapContainerClassName='map-container'></GoogleMap>
 }
