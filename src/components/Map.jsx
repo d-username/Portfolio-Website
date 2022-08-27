e0175531531083096474e7e02274349685c60dd1
David <czdavid93@gmail.com>
2022-08-27 15:37:21 +0200

    map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,10 +1,12 @@
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
 import './map.css';
+import {useMemo} from "react"
 
 export function Map() {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
   });
+
 
   if (!isLoaded) return <div>Loading...</div>
 
@@ -13,5 +15,22 @@ export function Map() {
 
 
 function TheMap() {
-  return <GoogleMap zoom={10} center={{lat:44, lng: -80}} mapContainerClassName='map-container'></GoogleMap>
+    const options = useMemo(
+      () => ({
+        disableDefaultUI: true,
+        clickableIcons: false,
+        mapId: 'e138e45c281430cd',
+      }),
+      []
+    );
+
+  return (
+    <GoogleMap
+      zoom={6}
+      center={{ lat: 51.507226, lng: -0.127579 }}
+      mapContainerClassName='map-container'
+      options={options}
+    >
+    </GoogleMap>
+  );
 }
