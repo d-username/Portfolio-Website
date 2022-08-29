e41aa853644b59cbb924f452941df7bb8243bf4f
David <czdavid93@gmail.com>
2022-08-30 00:38:50 +0200

    installed react-pdf

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,9 +1,27 @@
+import React, { useState } from 'react';
+import { Document, Page } from 'react-pdf';
+
 export function Resume() {
-    return (
-      <section id='resume'>
-        <div className='wrapper'>
-          <h1>resume.</h1>
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+  return (
+    <section id='resume'>
+      <div className='wrapper'>
+        <h1>resume.</h1>
+        <div>
+          <Document file={resume} onLoadSuccess={onDocumentLoadSuccess}>
+            <Page pageNumber={1} />
+          </Document>
+          <p>
+            Page {pageNumber} of {numPages}
+          </p>
         </div>
-      </section>
-    );
+      </div>
+    </section>
+  );
 }
