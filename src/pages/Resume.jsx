e074ad4bc96297f01723cdea940c302339a748d9
David <czdavid93@gmail.com>
2022-08-30 13:33:45 +0200

    scrollbar

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,27 +1,5 @@
-import React, { useState } from 'react';
-import { Document, Page } from 'react-pdf';
-
 export function Resume() {
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
-
-  return (
-    <section id='resume'>
-      <div className='wrapper'>
-        <h1>resume.</h1>
-        <div>
-          <Document file={resume} onLoadSuccess={onDocumentLoadSuccess}>
-            <Page pageNumber={1} />
-          </Document>
-          <p>
-            Page {pageNumber} of {numPages}
-          </p>
-        </div>
-      </div>
-    </section>
-  );
+  return(
+    <h1>resume.</h1>
+  )
 }
